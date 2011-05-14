69d6589fd53419a08be0e86d6ea1c35e51562c5d
Daniel Farina <daniel@heroku.com>
2011-05-13 18:20:26 -0700

    wip
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- wal_e/wal_set.py
+++ wal_e/wal_set.py
@@ -22,4 +22,48 @@ well as the types of files that can exist.
 
 
 """
+import blist
+import collections
 
+from bisect import bisect_left
+
+def interval_union(i1, i2):
+    return (min(i1[0], i2[0]), max(i1[1], i2[1]))
+
+def interval_disjoint_check(a):
+    last_end = None
+    for interval in a:
+        if interval[0] < last_end:
+            return False
+        last_end = interval[1]
+
+    return True
+
+def bin_scalar_in_intervals(a, x):
+    # While code is fresh put in this simple check
+    assert interval_disjoint_check(a)
+
+    i = bisect_left(a, (x,))
+
+    if i == len(a):
+        haystack_low, haystack_high = a[-1]
+        assert x >= haystack_low
+
+        if x <= haystack_high:
+            return a[-1]
+        else:
+            return None
+    elif i == 0:
+        haystack_low, haystack_high = a[0]
+        assert x <= haystack_low
+        if x == haystack_low:
+            return a[0]
+        else:
+            return None
+    else:
+        assert i > 0 and i < len(a)
+        haystack_low, haystack_high = a[i]
+
+        print 'foo', a[i]
+
+    assert False
