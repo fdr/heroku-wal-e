6d1e5c5efe3c6199441a1ab6c67c5e52c606e2ca
Daniel Farina <daniel@heroku.com>
2012-12-03 14:20:38 -0800

    PEP-8 cleanup
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- tests/test_log_help.py
+++ tests/test_log_help.py
@@ -1,17 +1,20 @@
 import wal_e.log_help as log_help
 
+
 def test_nonexisting_socket(tmpdir):
     # Must not raise an exception, silently failing is preferred for
     # now.
     log_help.configure(syslog_address=tmpdir.join('bogus'))
 
+
 def test_format_structured_info():
     zero = {}, ''
-    one = {'hello':'world'}, 'hello=world'
-    many = {'hello':'world', 'goodbye':'world'}, 'hello=world goodbye=world'
+    one = {'hello': 'world'}, 'hello=world'
+    many = {'hello': 'world', 'goodbye': 'world'}, 'hello=world goodbye=world'
 
     for d, expect in [zero, one, many]:
         assert log_help.WalELogger._fmt_structured(d) == expect
+
 
 def test_fmt_logline_simple():
     out = log_help.WalELogger.fmt_logline(
