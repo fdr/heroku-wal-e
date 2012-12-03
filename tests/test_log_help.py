0639853201d03370d74973a662da70a39aefa5c2
Daniel Farina <daniel@heroku.com>
2012-12-03 13:20:45 -0800

    Add structured output support to logging
    An optional 'structured' keyword argument taking a dictionary is now
    supported and logged (and is *not* escaped) in the key=value<SPC>
    format.
    
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- tests/test_log_help.py
+++ tests/test_log_help.py
@@ -1,6 +1,29 @@
-import wal_e.log_help
+import wal_e.log_help as log_help
 
 def test_nonexisting_socket(tmpdir):
     # Must not raise an exception, silently failing is preferred for
     # now.
-    wal_e.log_help.configure(syslog_address=tmpdir.join('bogus'))
+    log_help.configure(syslog_address=tmpdir.join('bogus'))
+
+def test_format_structured_info():
+    zero = {}, ''
+    one = {'hello':'world'}, 'hello=world'
+    many = {'hello':'world', 'goodbye':'world'}, 'hello=world goodbye=world'
+
+    for d, expect in [zero, one, many]:
+        assert log_help.WalELogger._fmt_structured(d) == expect
+
+def test_fmt_logline_simple():
+    out = log_help.WalELogger.fmt_logline(
+        'The message', 'The detail', 'The hint', {'structured-data': 'yes'})
+    assert out == """MSG: The message
+DETAIL: The detail
+HINT: The hint
+STRUCTURED: structured-data=yes"""
+
+    # Try without structured data
+    out = log_help.WalELogger.fmt_logline(
+        'The message', 'The detail', 'The hint')
+    assert out == """MSG: The message
+DETAIL: The detail
+HINT: The hint"""
